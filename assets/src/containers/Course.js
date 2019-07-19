b2da1a2acc6b286c3189589e20a5fd9bb81bde65
Justin Lee <justinlee0022@gmail.com>
2019-07-19 09:42:41 -0700

    merging master changes to files-develop

--- assets/src/containers/Course.js
+++ assets/src/containers/Course.js
@@ -47,7 +47,7 @@ function Course (props) {
               render={props => <AssignmentPlanning {...props} disabled={!courseInfo.course_view_options.ap}
                 courseId={courseId} />} />
             <Route path='/courses/:courseId/resources'
-              render={props => <ResourcesAccessed {...props} courseInfo={courseInfo}
+              render={props => <ResourcesAccessed {...props} disabled={!courseInfo.course_view_options.ap}
                 courseId={courseId} />} />
           </>
           : <Spinner />
